500675ac0d278558635acd27c236751b322b8fbb
root <root@vps258244.ovh.net>
2016-03-31 12:06:55 +0200

    nuevo

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ var port  	 = process.env.PORT || 8080; 			// Cogemos el puerto 8080
 // Configuracion
 mongoose.connect('mongodb://localhost:27017/MeanExample'); 	// Hacemos la conexión a la base de datos de Mongo con nombre "MeanExample"
 
-app.get('/', function(req, res) {
-    res.sendfile('./www/index.html');});
-
 app.configure(function() {
 	app.use(express.static(__dirname + '/angular')); 		
 	app.use(express.logger('dev')); 			// activamos el log en modo 'dev'
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+});
+app.use(express.static('www'));
+app.get('/', function (req, res) {
+    res.sendfile('./wwww/index.html');
 });
 
 // Cargamos los endpoints
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,4 @@
 'use strict';
-
-var MapApp = angular.module('MapApp', ['ionic','chart.js', 'races.controller','groups.controller',
-							'groupraces.controller','userlist.controller','messages.controller',
-							'backoffice.controller']);
-
-//var URL='https://localhost:3030/';
-//var URL='https://192.168.1.139:3030/';
-var URL='https://147.83.7.203:3030/';
-//var URL='https://10.189.28.37:3030/';
-
-/**
- * Routing table including associated controllers.
- */
-MapApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('menu', 			{url: "/map", abstract: true, templateUrl: "/templates/menu.html", controller: 'mainCtrl'})
-		.state('login', 		{url: '/login', templateUrl: '/templates/login.html', controller: 'loginCtrl'})
-		.state('menu.home', 	{url: '/home', views: {'menuContent': 				{templateUrl: '/templates/map.html', controller: 'GpsCtrl'}}})
-		.state('menu.groups', 	{url: '/groups', views: {'menuContent': 			{templateUrl: '/templates/groups.html', controller: 'GroupsCtrl'}}})
-		.state('menu.races', 	{url: '/races', views: {'menuContent': 			{templateUrl: '/templates/races.html', controller: 'RacesCtrl'}}})
-		.state('menu.group', 	{url: "/group/:parentId",views: {'menuContent': {templateUrl: "templates/messages.html",controller: 'MessagesCtrl'}}})
-		.state('menu.race', 	{url: "/race/:parentId",views: {'menuContent':  {templateUrl: "templates/messages.html",controller: 'MessagesCtrl'}}})
-		.state('menu.userlist', {url: "/users/:parent/:parentId",views: {'menuContent': {templateUrl: "templates/userlist.html",controller: 'UserListCtrl'}}})
-		.state('menu.racegroup', {url: "/groupraces/:id",views: {'menuContent': {templateUrl: "templates/groupraces.html",controller: 'groupRacesCtrl'}}})
-		.state('menu.profile', 	{url: '/profile', views: {'menuContent': 			{templateUrl: '/templates/profile3.html', controller: 'profileCtrl'}}})
-		.state('menu.logout', 	{url: '/logout', views: {'menuContent': 			{templateUrl: '/templates/logout.html', controller: 'logOutCtrl'}}})
-		.state('menu.stats', 	{url: '/stats', views: {'menuContent': 			{templateUrl: '/templates/stats.html', controller: 'statsCtrl'}}})
-		.state('menu.backoffice', {url: '/backoffice', views: {'menuContent': {templateUrl: "templates/backoffice.html", controller: 'backofficeCtrl'}}});
-	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/login');
-}]);
-
 /**
  * HEADER - handle menu toggle
  */
